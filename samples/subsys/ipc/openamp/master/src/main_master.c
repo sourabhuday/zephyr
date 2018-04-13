321cb46470980386dcd17955e7f228ce0ae271b1
Kumar Gala <kumar.gala@linaro.org>
2018-04-13 13:35:14 -0500

    changes for staging

--- samples/subsys/ipc/openamp/master/src/main_master.c
+++ samples/subsys/ipc/openamp/master/src/main_master.c
@@ -13,9 +13,7 @@
 #include <string.h>
 
 #include <openamp/open_amp.h>
-
-#include "platform.h"
-#include "resource_table.h"
+#include <metal/device.h>
 
 #define APP_TASK_STACK_SIZE (512)
 K_THREAD_STACK_DEFINE(thread_stack, APP_TASK_STACK_SIZE);
@@ -30,7 +28,78 @@ static K_SEM_DEFINE(message_received, 0, 1);
 static volatile unsigned int received_data;
 
 static struct rsc_table_info rsc_info;
-static struct hil_proc *proc;
+
+#define SHM_START_ADDRESS       0x04000400
+#define SHM_SIZE                0x7c00
+#define SHM_DEVICE_NAME         "sramx.shm"
+
+#define VRING_COUNT             2
+#define VRING_RX_ADDRESS        0x04000400
+#define VRING_TX_ADDRESS        0x04000800
+#define VRING_ALIGNMENT         4
+#define VRING_SIZE              32
+
+static metal_phys_addr_t shm_physmap[] = { SHM_START_ADDRESS };
+static struct metal_device shm_device = {
+	.name = SHM_DEVICE_NAME,
+	.bus = NULL,
+	.num_regions = 1,
+	{
+		{
+			.virt       = (void *) SHM_START_ADDRESS,
+			.physmap    = shm_physmap,
+			.size       = SHM_SIZE,
+			.page_shift = 0xffffffff,
+			.page_mask  = 0xffffffff,
+			.mem_flags  = 0,
+			.ops        = { NULL },
+		},
+	},
+	.node = { NULL },
+	.irq_num = 0,
+	.irq_info = NULL
+};
+
+#define RSC_TABLE_ADDRESS       0x04000000
+
+OPENAMP_PACKED_BEGIN
+struct lpc_resource_table {
+	uint32_t ver;
+	uint32_t num;
+	uint32_t reserved[2];
+	uint32_t offset[2];
+	struct fw_rsc_rproc_mem mem;
+	struct fw_rsc_vdev vdev;
+	struct fw_rsc_vdev_vring vring0, vring1;
+} OPENAMP_PACKED_END;
+
+struct lpc_resource_table *rsc_table_ptr = (void *) RSC_TABLE_ADDRESS;
+
+#if defined(CPU_LPC54114J256BD64_cm4)
+static const struct lpc_resource_table rsc_table = {
+	.ver = 1,
+	.num = 2,
+	.offset = {
+		offsetof(struct lpc_resource_table, mem),
+		offsetof(struct lpc_resource_table, vdev),
+	},
+	.mem = { RSC_RPROC_MEM, SHM_START_ADDRESS, SHM_START_ADDRESS, SHM_SIZE, 0 },
+	.vdev = { RSC_VDEV, VIRTIO_ID_RPMSG, 0, 1 << VIRTIO_RPMSG_F_NS, 0, 0, 0, VRING_COUNT, { 0, 0 } },
+	.vring0 = { VRING_TX_ADDRESS, VRING_ALIGNMENT, VRING_SIZE, 1, 0 },
+	.vring1 = { VRING_RX_ADDRESS, VRING_ALIGNMENT, VRING_SIZE, 2, 0 },
+};
+#endif
+
+void resource_table_init(void **table_ptr, int *length)
+{
+#if defined(CPU_LPC54114J256BD64_cm4)
+	/* Master: copy the resource table to shared memory. */
+	memcpy(rsc_table_ptr, &rsc_table, sizeof(struct lpc_resource_table));
+#endif
+
+	*length = sizeof(struct lpc_resource_table);
+	*table_ptr = rsc_table_ptr;
+}
 
 static void rpmsg_recv_callback(struct rpmsg_channel *channel, void *data,
 				int data_length, void *private, unsigned long src)
@@ -53,8 +122,12 @@ static void rpmsg_channel_deleted(struct rpmsg_channel *channel)
 
 static unsigned int receive_message(void)
 {
-	while (k_sem_take(&message_received, K_NO_WAIT) != 0)
+	while (k_sem_take(&message_received, K_NO_WAIT) != 0) {
+		;
+#if 0
 		hil_poll(proc, 0);
+#endif
+	}
 	return received_data;
 }
 
@@ -68,31 +141,27 @@ void app_task(void *arg1, void *arg2, void *arg3)
 	ARG_UNUSED(arg1);
 	ARG_UNUSED(arg2);
 	ARG_UNUSED(arg3);
+	int status = 0;
 
 	printk("\r\nOpenAMP demo started\r\n");
 
 	struct metal_init_params metal_params = METAL_INIT_DEFAULTS;
 	metal_init(&metal_params);
 
-	proc = platform_init(RPMSG_MASTER);
-	if (proc == NULL) {
-		printk("platform_init() failed\n");
+	status = metal_register_generic_device(&shm_device);
+	if (status != 0) {
+		printk("metal_register_generic_device(): could not register shared memory device: error code %d\n", status);
 		goto _cleanup;
 	}
 
 	resource_table_init((void **) &rsc_info.rsc_tab, &rsc_info.size);
 
-	struct remote_proc *rproc_ptr = NULL;
-	int status = remoteproc_resource_init(&rsc_info, proc,
-					      rpmsg_channel_created, rpmsg_channel_deleted,
-					      rpmsg_recv_callback, &rproc_ptr, RPMSG_MASTER);
-	if (status != 0) {
-		printk("remoteproc_resource_init() failed with status %d\n", status);
-		goto _cleanup;
-	}
-
-	while (k_sem_take(&channel_created, K_NO_WAIT) != 0)
+	while (k_sem_take(&channel_created, K_NO_WAIT) != 0) {
+#if 0
 		hil_poll(proc, 0);
+#endif
+		;
+	}
 
 	unsigned int message = 0;
 	status = send_message(message);
@@ -114,9 +183,6 @@ void app_task(void *arg1, void *arg2, void *arg3)
 	}
 
 _cleanup:
-	if (rproc_ptr) {
-		remoteproc_resource_deinit(rproc_ptr);
-	}
 	metal_finish();
 
 	printk("OpenAMP demo ended.\n");
